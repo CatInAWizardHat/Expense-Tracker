6fe1646496cf0ee3d5a5df26f8c1debe40ffae68
Alex <{ID}+{username}@users.noreply.github.com>
2025-06-07 16:19:38 -0700

    Created read function, added addTransaction signature to ExpenseTracker

--- ExpenseTracker.java
+++ ExpenseTracker.java
@@ -1,8 +1,27 @@
 package Expense-Tracker;
 import java.util.ArrayList;
+import java.util.IOException;
+import java.util.Scanner;
 
 public class ExpenseTracker {
     private ArrayList<Transaction> transactions;
+    private File transactionFile;
+
+    public void addTransaction() {
+
+    }
+
+    private void read(File file) throws IOException {
+        Scanner sc = new Scanner(file);
+
+        while(scanner.hasNext()) {
+            String[] tokens = scanner.nextLine.split(',');
+            int id = tokens[0];
+            string name = tokens[1];
+            double price = tokens[2];
+            string date = tokens[3];
+        }
+    }
 
     public ExpenseTracker() {
         try {
