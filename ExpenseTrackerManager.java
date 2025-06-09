9fd6b2d18536c5427dd9a03702a58f7969f9fdcf
Alex <{ID}+{username}@users.noreply.github.com>
2025-06-08 22:17:46 -0700

    Added QUIT constant to ExpenseTrackerManager and added to VALID_CHOICES constant

--- ExpenseTrackerManager.java
+++ ExpenseTrackerManager.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 
 public class ExpenseTrackerManager {
 
-    static final String VALID_CHOICES = "cup";
+    static final String VALID_CHOICES = "cupq";
     static final String CREATE_RECORD = "c";
     static final String UPDATE_RECORD = "u";
     static final String PRINT_RECORDS = "p";
+    static final String QUIT = "q";
 
     public static Scanner sc = new Scanner(System.in);
 
