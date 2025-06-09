464c2d847a0dac2b218612e1a8ec839b694e09a3
Alex <{ID}+{username}@users.noreply.github.com>
2025-06-08 21:55:03 -0700

    Moved file handling logic to ExpenseTracker

--- ExpenseTrackerManager.java
+++ ExpenseTrackerManager.java
@@ -1,34 +1,13 @@
-package Expense-Tracker;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.time.LocalDate;
 import java.util.Scanner;
 
 public class ExpenseTrackerManager {
-    final static string VALID_CHOICES="cup"
-    final static string CREATE_RECORD="c";
-    final static string UPDATE_RECORD="u";
-    final static string PRINT_RECORDS="p";
+
+    static final String VALID_CHOICES = "cup";
+    static final String CREATE_RECORD = "c";
+    static final String UPDATE_RECORD = "u";
+    static final String PRINT_RECORDS = "p";
 
     public static Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        try {
-            File expenses = new File("expense_record.txt");
-            if (expenses.createNewFile()) {
-                System.out.println("File created.");
-            } else {
-                System.out.println("File already exists.");
-            }
-
-        } catch (IOException e) {
-            System.out.println(e.toString());
-            e.printStackTrace();
-        }
-    }
+    public static void main(String[] args) {}
 }
